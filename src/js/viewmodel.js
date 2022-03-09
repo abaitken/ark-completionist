348cf32f22cc793207d44fbf341477f9b137961d
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-03-09 11:52:07 +0000

    Sorted by distance

--- src/js/viewmodel.js
+++ src/js/viewmodel.js
@@ -3,52 +3,81 @@ var NOTE_TYPES = {
 	DOSSIER: 1
 };
 
-function ViewModel()
-{
-    var self = this;    
-    self.dataReady = ko.observable(false);
-    self.messages = ko.observable('Fetching...');
+function Compare(left, right) {
+	if (left < right)
+		return -1;
+
+	if (left > right)
+		return 1;
+
+	return 0;
+}
+
+function ViewModel() {
+	var self = this;
+	self.dataReady = ko.observable(false);
+	self.messages = ko.observable('Fetching...');
 	self.notes = ko.observableArray([]);
 	self._foundData = new FoundNotesData();
-	self.SaveValue = function(item) {
+	self.SaveValue = function (item) {
 		var newValue = item.Found();
 		self._foundData.SetFound(item._type, item._index, newValue);
 	};
 	self.HideFound = ko.observable(true);
+	self.SortByDistance = ko.observable(true);
 	self.MyLon = ko.observable(50.0);
 	self.MyLat = ko.observable(50.0);
-    
-    self.Init = function ()
-    {
-        ko.applyBindings(self);
-		$.ajax({
-				type: 'GET',
-				url: 'data.json',
-				dataType: 'json',
-				mimeType: 'application/json',
-				success: function (data) {
-					let notes = [];
-					for (let index = 0; index < data['notes'].length; index++) {
-						const element = data['notes'][index];
-						notes.push(new NoteItem(self, element, self._foundData.GetFound(NOTE_TYPES.NOTE, index), NOTE_TYPES.NOTE, index));
-					}
-					for (let index = 0; index < data['dossiers'].length; index++) {
-						const element = data['dossiers'][index];
-						notes.push(new NoteItem(self, element, self._foundData.GetFound(NOTE_TYPES.DOSSIER, index), NOTE_TYPES.DOSSIER, index));
-					}
+	self.sortedNotes = ko.computed(function () {
+		var notes = self.notes();
 
-					self.notes(notes);
-					self.dataReady(true);
-				},
-				error: function(jqXHR, textStatus, errorThrown) {
-					self.messages(errorThrown);
-					$('#messages').attr("class","alert alert-danger");
-					// TODO : Text not displaying correctly
-					$('#track-info').html("Error: " + errorThrown);
-				}
+		if (self.SortByDistance()) {
+			notes.sort(function (left, right) {
+				return Compare(left.Distance(), right.Distance());
 			});
-	
-    };
+		} else {
+			notes.sort(function (left, right) {
+				let typeCompare = Compare(left._type, right._type);
+				if(typeCompare === 0)
+					return Compare(left._index, right._index);
+
+				return typeCompare;
+			});
+
+		}
+
+		return notes;
+	}, self);
+
+	self.Init = function () {
+		ko.applyBindings(self);
+		$.ajax({
+			type: 'GET',
+			url: 'data.json',
+			dataType: 'json',
+			mimeType: 'application/json',
+			success: function (data) {
+				let notes = [];
+				for (let index = 0; index < data['notes'].length; index++) {
+					const element = data['notes'][index];
+					notes.push(new NoteItem(self, element, self._foundData.GetFound(NOTE_TYPES.NOTE, index), NOTE_TYPES.NOTE, index));
+				}
+				for (let index = 0; index < data['dossiers'].length; index++) {
+					const element = data['dossiers'][index];
+					notes.push(new NoteItem(self, element, self._foundData.GetFound(NOTE_TYPES.DOSSIER, index), NOTE_TYPES.DOSSIER, index));
+				}
+
+				self.notes(notes);
+				self.dataReady(true);
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				self.messages(errorThrown);
+				$('#messages').attr("class", "alert alert-danger");
+				// TODO : Text not displaying correctly
+				$('#track-info').html("Error: " + errorThrown);
+			}
+		});
+
+	};
 }
 
 function NoteItem(parent, data, found, type, index) {
@@ -57,37 +86,37 @@ function NoteItem(parent, data, found, type, index) {
 	self._index = index;
 	self._parent = parent;
 	self.Coordinates = 'lat: ' + self._inner.lat + ', lon: ' + self._inner.lon;
-	self.CompassDirection = ko.computed(function(){
+	self.CompassDirection = ko.computed(function () {
 		let result = '';
 		result += (self._inner.lat < parent.MyLat()) ? 'N' : 'S';
 		result += (self._inner.lon > parent.MyLon()) ? 'E' : 'W';
 		return result;
 	}, self);
 	self.Found = ko.observable(found);
-	self.Found.subscribe(function(newValue){
+	self.Found.subscribe(function (newValue) {
 		parent.SaveValue(self);
 	});
-	self.Distance = ko.computed(function() {
+	self.Distance = ko.computed(function () {
 		let a = Math.abs(self._inner.lat - parent.MyLat());
 		let b = Math.abs(self._inner.lon - parent.MyLon());
 		let c = Math.pow(a, 2) + Math.pow(b, 2);
 		return '~' + Math.round(Math.sqrt(c)) + ' units';
 	}, self);
 	self._type = type;
-	self.Name = ko.computed(function() {
+	self.Name = ko.computed(function () {
 		let result = self._inner.name;
-		if(self._inner.author)
+		if (self._inner.author)
 			result = self._inner.author + ' ' + result;
-		if(self._type === NOTE_TYPES.DOSSIER)
+		if (self._type === NOTE_TYPES.DOSSIER)
 			result = 'Dossier: ' + result;
 		return result;
 	}, self);
-	self.CaveText = ko.computed(function() {
-		if(self._inner['in-cave'])
+	self.CaveText = ko.computed(function () {
+		if (self._inner['in-cave'])
 			return self._inner['in-cave'];
 		return '';
 	}, self);
-	self.IsHidden = ko.computed(function() {
+	self.IsHidden = ko.computed(function () {
 		return self.Found() && self._parent.HideFound();
 	}, self);
 }
@@ -97,28 +126,26 @@ function FoundNotesData() {
 
 	let data = localStorage.getItem('foundNotes');
 
-	if(data)
-	{
+	if (data) {
 		self._inner = JSON.parse(data);
 	}
-	else
-	{
-		self._inner = { };
-		self._inner['type' + NOTE_TYPES.NOTE] = { };
-		self._inner['type' + NOTE_TYPES.DOSSIER] = { };
+	else {
+		self._inner = {};
+		self._inner['type' + NOTE_TYPES.NOTE] = {};
+		self._inner['type' + NOTE_TYPES.DOSSIER] = {};
 	}
 
-	self.GetFound = function(type, key) {
+	self.GetFound = function (type, key) {
 		let hive = self._inner['type' + type];
 		let value = hive['key' + key];
-		if(value)
+		if (value)
 			return value;
 		return false;
 	}
 
-	self.SetFound = function(type, key, value) {
+	self.SetFound = function (type, key, value) {
 		let hive = self._inner['type' + type];
-		if(value)
+		if (value)
 			hive['key' + key] = value;
 		else
 			delete hive['key' + key];
